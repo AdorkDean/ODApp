783f8a78c3e9840f58ea25968e052c239c62d60f
刘培壮 <lpz191@live.cn>
2016-01-19 17:48:06 +0800

    更改目录结构

--- ODApp/Utility/MyControl/ODClassMethod.h
+++ ODApp/Utility/MyControl/ODClassMethod.h
@@ -39,7 +39,7 @@
                                tag:(NSInteger)tag
                              image:(NSString *)name
                              title:(NSString *)title
-                              font:(NSInteger)size;
+                              font:(NSInteger)size ;
 //创建UIImageView
 + (UIImageView *)creatImageViewWithFrame:(CGRect)frame
                                imageName:(NSString *)name tag:(NSInteger)tag;
