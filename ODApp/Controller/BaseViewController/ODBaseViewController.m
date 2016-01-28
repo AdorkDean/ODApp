7d1097d85027a371125e7948d2b01d3e39cd2539
刘培壮 <lpz191@live.cn>
2016-01-28 17:30:41 +0800

    精简代码

--- ODApp/Controller/BaseViewController/ODBaseViewController.m
+++ ODApp/Controller/BaseViewController/ODBaseViewController.m
@@ -16,7 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
 }
 
 - (void)addTitleViewWithName:(NSString *)name {
--- ODApp/Controller/NavigationController/ODNavigationController.m
+++ ODApp/Controller/NavigationController/ODNavigationController.m
@@ -16,28 +16,27 @@
 
 @implementation ODNavigationController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     self.navigationBar.barTintColor = [UIColor colorWithHexString:@"#f3f3f3" alpha:1];
     _touchDelegate = self.interactivePopGestureRecognizer.delegate;
     self.delegate = self;
-
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
 {
-    self.tabBarController.tabBar.hidden = YES;
     [super pushViewController:viewController animated:animated];
+    self.tabBarController.tabBar.hidden = YES;
 }
 
 #pragma mark - UINavigationControllerDelegate
-
 - (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
 {
     if (viewController == self.childViewControllers[0])
     {
         self.interactivePopGestureRecognizer.delegate = _touchDelegate;
-        self.tabBarController.tabBar.hidden = NO;
+        viewController.tabBarController.tabBar.hidden = NO;
     }
 }
 
