0dde0271885d524a354ad4e06d5bf074db15faf8
刘培壮 <lpz191@live.cn>
2016-01-31 16:34:55 +0800

    添加navigationBar创建方式

---
+++ ODApp/View/BaseView/ODBarButton.m
@@ -0,0 +1,60 @@
+//
+//  ODBarButton.m
+//  ODApp
+//
+//  Created by 刘培壮 on 16/1/31.
+//  Copyright © 2016年 Odong-YG. All rights reserved.
+//
+
+#import "ODBarButton.h"
+
+@implementation ODBarButton
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    [self.imageView sizeToFit];
+    [self.titleLabel sizeToFit];
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
+    
+    switch (self.barButtonType)
+    {
+        case ODBarButtonImageLeft:
+        {
+            self.imageView.od_x = 0;
+            self.imageView.od_y = (self.od_height - self.imageView.od_height) / 2;
+            self.titleLabel.od_x = (self.od_width + CGRectGetMaxX(self.imageView.frame) - self.titleLabel.od_width) / 2;
+            self.titleLabel.od_y = (self.od_height - self.titleLabel.od_height) / 2;
+        }
+            break;
+        case ODBarButtonImageUp:
+        {
+            self.imageView.od_y = 0;
+            self.imageView.od_x = (self.od_width - self.imageView.od_width) / 2;
+            self.titleLabel.od_y = (self.od_height + CGRectGetMaxY(self.titleLabel.frame) - self.imageView.od_height) / 2;
+            self.titleLabel.od_x = (self.od_width - self.titleLabel.od_width) / 2;
+        }
+            break;
+        case ODBarButtonTextLeft:
+        {
+            self.titleLabel.od_x = 0;
+            self.titleLabel.od_y = (self.od_height - self.titleLabel.od_height) / 2;
+            self.imageView.od_x = (self.od_width + CGRectGetMaxX(self.titleLabel.frame) - self.imageView.od_width) / 2;
+            self.imageView.od_y = (self.od_height - self.imageView.od_height) / 2;
+        }
+            break;
+        case ODBarButtonTextUp:
+        {
+            self.titleLabel.od_y = 0;
+            self.titleLabel.od_x = (self.od_width - self.titleLabel.od_width) / 2;
+            self.imageView.od_y = (self.od_height + CGRectGetMaxY(self.imageView.frame) - self.titleLabel.od_height) / 2;
+            self.imageView.od_x = (self.od_width - self.imageView.od_width) / 2;
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+@end
