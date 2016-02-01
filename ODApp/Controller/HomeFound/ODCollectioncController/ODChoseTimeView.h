22b0bc03300e084298e24b8fab3c0f20177b5137
zhz <1657291066@qq.com>
2016-02-01 20:43:57 +0800

    -地址左滑效果

---
+++ ODApp/Controller/HomeFound/ODCollectioncController/ODChoseTimeView.h
@@ -0,0 +1,13 @@
+//
+//  ODChoseTimeView.h
+//  ODApp
+//
+//  Created by zhz on 16/2/1.
+//  Copyright © 2016年 Odong-YG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODChoseTimeView : UIView
+
+@end
