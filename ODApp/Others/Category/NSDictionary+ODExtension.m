d000bc4c42aedb730aefbda61fcf7f3bad94513c
刘培壮 <lpz191@live.cn>
2016-02-03 11:09:19 +0800

    修复启动图片

--- ODApp/Others/Category/NSDictionary+ODExtension.m
+++ ODApp/Others/Category/NSDictionary+ODExtension.m
@@ -19,32 +19,26 @@
     [self enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
         
         // 类型经常变，抽出来
-        NSString *type;
-        NSString *desc;
-        NSString *star = @"";
+        NSString *type = @"<#type#>";
+        NSString *desc = @"<#decription#>";
+        NSString *star = @"*";
         if ([obj isKindOfClass:NSClassFromString(@"__NSCFString")]) {
             type = @"NSString";
             desc = @"copy";
-            star = @"*";
         }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFArray")]){
             type = @"NSArray";
             desc = @"strong";
-            star = @"*";
         }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFDictionary")]){
             type = @"NSDictionary";
             desc = @"strong";
-            star = @"*";
         }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFNumber")]){
             type = @"int";
             desc = @"assign";
+            star = @"";
         }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFBoolean")]){
             type = @"Bool";
             desc = @"assign";
-        }
-        else
-        {
-            type = @"<#type#>";
-            desc = @"<#decription#>";
+            star = @"";
         }
         // 属性字符串
         NSString *str = [NSString stringWithFormat:@"@property (nonatomic, %@) %@ %@%@;",desc,type,star,key];
