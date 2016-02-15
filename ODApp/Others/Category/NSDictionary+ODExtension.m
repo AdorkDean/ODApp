fd9353614e9c4c747385ba0cf9702bdc1f5c3912
刘培壮 <lpz191@live.cn>
2016-02-15 10:52:13 +0800

    。。

--- ODApp/Others/Category/NSDictionary+ODExtension.m
+++ ODApp/Others/Category/NSDictionary+ODExtension.m
@@ -14,39 +14,44 @@
     
     // 拼接属性字符串代码
     NSMutableString *strM = [NSMutableString string];
-    
-    // 1.遍历字典，把字典中的所有key取出来，生成对应的属性代码
-    [self enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+    @try {
+        // 1.遍历字典，把字典中的所有key取出来，生成对应的属性代码
+        [self enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+            
+            // 类型经常变，抽出来
+            NSString *type = @"<#type#>";
+            NSString *desc = @"<#decription#>";
+            NSString *star = @"*";
+            if ([obj isKindOfClass:NSClassFromString(@"__NSCFString")]) {
+                type = @"NSString";
+                desc = @"copy";
+            }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFArray")]){
+                type = @"NSArray";
+                desc = @"strong";
+            }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFDictionary")]){
+                type = @"NSDictionary";
+                desc = @"strong";
+            }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFNumber")]){
+                type = @"int";
+                desc = @"assign";
+                star = @"";
+            }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFBoolean")]){
+                type = @"Bool";
+                desc = @"assign";
+                star = @"";
+            }
+            // 属性字符串
+            NSString *str = [NSString stringWithFormat:@"@property (nonatomic, %@) %@ %@%@;",desc,type,star,key];
+            // 每生成属性字符串，就自动换行。
+            [strM appendFormat:@"\n%@\n",str];
+        }];
         
-        // 类型经常变，抽出来
-        NSString *type = @"<#type#>";
-        NSString *desc = @"<#decription#>";
-        NSString *star = @"*";
-        if ([obj isKindOfClass:NSClassFromString(@"__NSCFString")]) {
-            type = @"NSString";
-            desc = @"copy";
-        }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFArray")]){
-            type = @"NSArray";
-            desc = @"strong";
-        }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFDictionary")]){
-            type = @"NSDictionary";
-            desc = @"strong";
-        }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFNumber")]){
-            type = @"int";
-            desc = @"assign";
-            star = @"";
-        }else if ([obj isKindOfClass:NSClassFromString(@"__NSCFBoolean")]){
-            type = @"Bool";
-            desc = @"assign";
-            star = @"";
-        }
-        // 属性字符串
-        NSString *str = [NSString stringWithFormat:@"@property (nonatomic, %@) %@ %@%@;",desc,type,star,key];
-        // 每生成属性字符串，就自动换行。
-        [strM appendFormat:@"\n%@\n",str];
-    }];
-    
-    // 把拼接好的字符串打印出来，就好了。
-    NSLog(@"property == /n%@",strM);
+        // 把拼接好的字符串打印出来，就好了。
+        NSLog(@"property == /n%@",strM);
+    }
+    @catch (NSException *exception) {
+        NSLog(@"json数据异常");
+    }
+   
 }
 @end
