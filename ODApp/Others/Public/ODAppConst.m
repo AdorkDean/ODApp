888933ece3375d5ede119738df39d8efbe9cea1a
刘培壮 <lpz191@live.cn>
2016-02-18 19:56:19 +0800

    更改规范

--- ODApp/Others/Public/ODAppConst.m
+++ ODApp/Others/Public/ODAppConst.m
@@ -38,9 +38,9 @@ NSString *const ODLocationApiKey = @"82b3b9feaca8b2c33829a156672a5fd0";
 #pragma mark - 请求URL接口
 /** 统一的URL */
 #ifdef DEBUG
-NSString * const ODBaseURL = @"http://woquapi.test.odong.com/1.0/";
+NSString * const ODBaseURL = @"http://woquapi.test.odong.com";
 #else
-NSString * const ODBaseURL = @"http://woquapi.test.odong.com/1.0/";
+NSString * const ODBaseURL = @"http://woquapi.test.odong.com";
 #endif
 
 NSString * const kBazaarUnlimitTaskUrl = @"http://woquapi.test.odong.com/1.0/task/list";
@@ -72,8 +72,8 @@ NSString * const kHomeFoundPictureUrl = @"http://woquapi.test.odong.com/1.0/othe
 
 NSString * const ODSkillDetailUrl = @"http://woquapi.test.odong.com/1.0/swap/info";
 
-NSString * const ODCityListUrl = @"other/city/list";
-NSString * const ODHomeFoundUrl = @"other/home";
+NSString * const ODCityListUrl = @"/1.0/other/city/list";
+NSString * const ODHomeFoundUrl = @"/1.0/other/home";
 NSString * const ODHomeChangeSkillUrl = @"http://woquapi.test.odong.com/1.0/other/home";
 NSString *const ODFindJobUrl = @"http://www.myjob500.com/user/extloginpf";
 NSString * const ODHotActivityUrl = @"http://woquapi.test.odong.com/1.0/doc/store/activity/list";
@@ -92,9 +92,9 @@ NSString * const kEditeAddressUrl = @"http://woquapi.test.odong.com/1.0/user/add
 NSString * const kGetServecTimeUrl = @"http://woquapi.test.odong.com/1.0/swap/service/time";
 NSString * const kSaveOrderUrl = @"http://woquapi.test.odong.com/1.0/swap/order";
 
-NSString * const KActivityListUrl = @"store/activity/list";
-NSString * const KActivityDetailUrl = @"store/apply/detail2";
-NSString * const KActivityApplyUrl = @"store/activity/apply";
+NSString * const KActivityListUrl = @"/1.0/store/activity/list";
+NSString * const KActivityDetailUrl = @"/1.0/store/apply/detail2";
+NSString * const KActivityApplyUrl = @"/1.0/store/activity/apply";
 
 NSString * const kGetLikeListUrl = @"http://woquapi.test.odong.com/1.0/swap/love/list";
 NSString * const kGetMyOrderListUrl = @"http://woquapi.test.odong.com/1.0/swap/order/list";
@@ -119,11 +119,11 @@ NSString * const kGetApplyListUrl = @"http://woquapi.test.odong.com/1.0/store/ap
 NSString * const kGiveOpinionUrl = @"http://woquapi.test.odong.com/1.0/other/feedback";
 NSString * const kCollectionUrl = @"http://woquapi.test.odong.com/1.0/user/love/list";
 
-NSString * const kCallbackUrl = @"other/share/callback";
+NSString * const kCallbackUrl = @"/1.0/other/share/callback";
 
 
 
-NSString * const ODPersonalReleaseTaskUrl = @"swap/list";
+NSString * const ODPersonalReleaseTaskUrl = @"/1.0/swap/list";
 
 
 
