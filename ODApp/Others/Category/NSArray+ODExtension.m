1affe83095d32e5efe81f89dcf0f5edd50c787d2
刘培壮 <lpz191@live.cn>
2016-02-22 18:19:52 +0800

    NSArray

---
+++ ODApp/Others/Category/NSArray+ODExtension.m
@@ -0,0 +1,27 @@
+//
+//  NSArray+ODExtension.m
+//  ODApp
+//
+//  Created by 刘培壮 on 16/2/22.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import "NSArray+ODExtension.h"
+
+@implementation NSArray (ODExtension)
+- (NSString *)description
+{
+    // 1.定义字符创保存拼接结果
+    NSMutableString *strM = [NSMutableString string];
+    [strM appendFormat:@"[\n"];
+    
+    // 2.遍历字典，拼接字典中的键值对
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [strM appendFormat:@"%@",obj];
+    }];
+    
+    [strM appendFormat:@"]\n"];
+    return strM;
+
+}
+@end
