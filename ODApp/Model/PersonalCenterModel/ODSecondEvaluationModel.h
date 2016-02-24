e423987e1086eeb86ec5f6ab2181057374eb1827
zhz <1657291066@qq.com>
2016-02-24 17:01:11 +0800

    完善订单

---
+++ ODApp/Model/PersonalCenterModel/ODSecondEvaluationModel.h
@@ -0,0 +1,23 @@
+//
+//  ODSecondEvaluationModel.h
+//  ODApp
+//
+//  Created by zhz on 16/2/24.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ODSecondEvaluationModel : NSObject
+
+@property(nonatomic,copy)NSString *id;
+@property(nonatomic,copy)NSString *obj_id;
+@property(nonatomic,copy)NSString *reason;
+@property(nonatomic,copy)NSString *reason_num;
+@property(nonatomic,copy)NSString *created_at;
+
+
+
+
+
+@end
