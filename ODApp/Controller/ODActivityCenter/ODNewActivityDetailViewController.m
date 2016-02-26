d9b2d1b60e5b08826f5e1139022c5fa5a608401a
刘培壮 <liupz@odong.com>
2016-02-26 22:10:21 +0800

    bug

--- ODApp/Controller/ODActivityCenter/ODNewActivityDetailViewController.m
+++ ODApp/Controller/ODActivityCenter/ODNewActivityDetailViewController.m
@@ -252,9 +252,11 @@ static NSString * const detailInfoCell = @"detailInfoCell";
         webView.delegate = self;
         webView.layer.masksToBounds = YES;
         webView.layer.cornerRadius = 5;
-        webView.layer.borderColor = [UIColor colorWithHexString:@"d0d0d0" alpha:1].CGColor;
+//        webView.layer.borderColor = [UIColor colorWithHexString:@"d0d0d0" alpha:1].CGColor;
+        webView.backgroundColor = [UIColor clearColor];
         webView.scrollView.showsHorizontalScrollIndicator = NO;
         webView.scrollView.scrollEnabled = NO;
+        
         [self.baseScrollV addSubview:webView];
         _webView = webView;
     }
@@ -270,6 +272,7 @@ static NSString * const detailInfoCell = @"detailInfoCell";
         [view.goodBtn addTarget:self action:@selector(clickGood:) forControlEvents:UIControlEventTouchUpInside];
         view.frame = CGRectMake(0, CGRectGetMaxY(self.webView.frame), KScreenWidth, 50);
         [self.baseScrollV addSubview:view];
+        [self.baseScrollV bringSubviewToFront:_bottomButtonView];
         _bottomButtonView = view;
     }
     return _bottomButtonView;
@@ -304,8 +307,6 @@ static NSString * const detailInfoCell = @"detailInfoCell";
     self.navigationItem.title = @"活动详情";
     [ODNewActivityCenterViewController sharedODNewActivityCenterViewController].needRefresh = NO;
     self.navigationItem.rightBarButtonItem = [UIBarButtonItem OD_itemWithTarget:self action:@selector(share:) image:[UIImage imageNamed:@"话题详情-分享icon"] highImage:nil];
-
-    
     
     [self requestData];
 }
@@ -504,6 +505,7 @@ static NSString * const detailInfoCell = @"detailInfoCell";
         [self.bottomButtonView addLineOnBottom];
         hasload = YES;
     }
+    webView.layer.backgroundColor = [UIColor clearColor].CGColor;
     sharedTimes = self.resultModel.share_cnt;
     [[self.bottomButtonView shareBtn]setTitle:[NSString stringWithFormat:@"分享 %d",self.resultModel.share_cnt] forState:UIControlStateNormal];
     [[self.bottomButtonView goodBtn]setTitle:[NSString stringWithFormat:@"赞 %d",self.resultModel.love_cnt] forState:UIControlStateNormal];
@@ -626,7 +628,6 @@ static NSString * const detailInfoCell = @"detailInfoCell";
         NSDictionary *infoDic = [NSDictionary dictionaryWithObjectsAndKeys:[@(self.resultModel.activity_id)stringValue],@"obj_id",@"4" ,@"type",@"微信",@"share_platform", nil];
         [ODHttpTool getWithURL:kCallbackUrl parameters:infoDic modelClass:[NSObject class] success:^(id model)
          {
-            [ODProgressHUD showSuccessWithStatus:@"分享成功"];
              
          }
                        failure:^(NSError *error)
