f387f779cf5c07aade6021c6fb4c04276610fd42
wanghuida <wanghuida258@aliyun.com>
2016-03-01 13:08:37 +0800

    注册界面修改

--- ODApp/View/ODLand/ODRegisteredView.m
+++ ODApp/View/ODLand/ODRegisteredView.m
@@ -7,6 +7,7 @@
 //
 
 #import "ODRegisteredView.h"
+#import <Masonry.h>
 
 @implementation ODRegisteredView
 
@@ -15,15 +16,27 @@
     ODRegisteredView *view =  [[[NSBundle mainBundle] loadNibNamed:@"ODRegisteredView" owner:nil options:nil] firstObject];
     
     
-   view.backgroundColor = [UIColor whiteColor];
-   view.registereButton.layer.masksToBounds = YES;
-   view.registereButton.layer.cornerRadius = 5;
-   view.registereButton.layer.borderColor = [UIColor colorWithHexString:@"#b0b0b0" alpha:1].CGColor;
-   view.registereButton.layer.borderWidth = 1;
-   view.password.secureTextEntry = YES;
-   view.phoneNumber.keyboardType = UIKeyboardTypeNumberPad;
+    view.backgroundColor = [UIColor whiteColor];
+    
+    view.registereButton.layer.masksToBounds = YES;
+    view.registereButton.layer.cornerRadius = 5;
+    view.registereButton.layer.borderColor = [UIColor colorWithHexString:@"#333333" alpha:1].CGColor;
+    view.registereButton.layer.borderWidth = 0.5f;
+    
+    view.password.secureTextEntry = YES;
+    view.phoneNumber.keyboardType = UIKeyboardTypeNumberPad;
     view.password.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
-       
+    
+    [view.line1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(0.5f);
+    }];
+    [view.line2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(0.5f);
+    }];
+    [view.line3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(0.5f);
+    }];
+    
 
     return view;
     
