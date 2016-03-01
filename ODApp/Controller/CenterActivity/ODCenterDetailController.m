cc7f0bb3465d461d6a1f2ce30d9523694ce9a9ad
sdhou <xiaohou.don@gmail.com>
2016-03-01 10:38:47 +0800

    format url

--- ODApp/Controller/CenterActivity/ODCenterDetailController.m
+++ ODApp/Controller/CenterActivity/ODCenterDetailController.m
@@ -22,62 +22,62 @@
 #import "ODUserInformation.h"
 #import "ODPersonalCenterViewController.h"
 #import "ODCenterPactureController.h"
+
 int pageNumnber = 0;
 
-@interface ODCenterDetailController ()<UITableViewDataSource , UITableViewDelegate>
+@interface ODCenterDetailController () <UITableViewDataSource, UITableViewDelegate>
 
-@property (nonatomic , strong) UITableView *tableView;
-@property (nonatomic , strong) ODCenderDetailView *centerDetailView;
-@property(nonatomic,strong)AFHTTPRequestOperationManager *manager;
-@property(nonatomic,strong)AFHTTPRequestOperationManager *managers;
-@property(nonatomic,strong)CenterDetailModel *model;
-@property(nonatomic,strong)UIPageControl *pageControl;
-@property (nonatomic , strong) UILabel *centerNameLabe;
-@property(nonatomic,strong) NSTimer *myTimer;
+@property(nonatomic, strong) UITableView *tableView;
+@property(nonatomic, strong) ODCenderDetailView *centerDetailView;
+@property(nonatomic, strong) AFHTTPRequestOperationManager *manager;
+@property(nonatomic, strong) AFHTTPRequestOperationManager *managers;
+@property(nonatomic, strong) CenterDetailModel *model;
+@property(nonatomic, strong) UIPageControl *pageControl;
+@property(nonatomic, strong) UILabel *centerNameLabe;
+@property(nonatomic, strong) NSTimer *myTimer;
 
 
 @end
 
 @implementation ODCenterDetailController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"中心详情";
     [self getData];
 }
 
 #pragma mark - lifeCycle
-- (void)viewWillAppear:(BOOL)animated
-{
+
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     pageNumnber = 0;
     self.myTimer = [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(timerFired) userInfo:nil repeats:YES];
-    [[NSRunLoop mainRunLoop]addTimer:self.myTimer forMode:NSRunLoopCommonModes];
-    
-    
+    [[NSRunLoop mainRunLoop] addTimer:self.myTimer forMode:NSRunLoopCommonModes];
+
+
 }
 
 
-- (void)viewWillDisappear:(BOOL)animated
-{
+- (void)viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
     [self.myTimer invalidate];
     self.myTimer = nil;
-    
+
 }
+
 #pragma mark - 点击事件
-- (void)appointmentAction:(UIButton *)sender
-{
-    
+
+- (void)appointmentAction:(UIButton *)sender {
+
     if ([[ODUserInformation sharedODUserInformation].openID isEqualToString:@""]) {
-        
+
         ODPersonalCenterViewController *vc = [[ODPersonalCenterViewController alloc] init];
         [self presentViewController:vc animated:YES completion:nil];
-        
-        
-    }else{
-        
+
+
+    } else {
+
         ODCenterYuYueController *vc = [[ODCenterYuYueController alloc] init];
         vc.centerName = self.model.name;
         vc.storeId = self.storeId;
@@ -85,72 +85,67 @@ int pageNumnber = 0;
         [self.navigationController pushViewController:vc animated:YES];
 
     }
-  }
+}
 
-- (void)phoneAction:(UITapGestureRecognizer *)sender
-{
-        
-    NSMutableString *str=[[NSMutableString alloc] initWithFormat:@"tel:%@",self.centerDetailView.phoneLabel.text];
-    
+- (void)phoneAction:(UITapGestureRecognizer *)sender {
+
+    NSMutableString *str = [[NSMutableString alloc] initWithFormat:@"tel:%@", self.centerDetailView.phoneLabel.text];
+
     UIWebView *callWebview = [[UIWebView alloc] init];
     [callWebview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:str]]];
     [self.view addSubview:callWebview];
-    
-    
+
+
 }
 
 
-- (void)addressAction:(UITapGestureRecognizer *)sender
-{
-   
+- (void)addressAction:(UITapGestureRecognizer *)sender {
+
     ODCenterPactureController *vc = [[ODCenterPactureController alloc] init];
-    
-      NSString *webUrl = [NSString stringWithFormat:@"http://h5.odong.com/map/search?lng=%@&lat=%@" , self.model.lng , self.model.lat];
-    
+
+    NSString *webUrl = [NSString stringWithFormat:@"http://h5.odong.com/map/search?lng=%@&lat=%@", self.model.lng, self.model.lat];
+
     vc.webUrl = webUrl;
     vc.activityName = self.model.name;
     [self.navigationController pushViewController:vc animated:YES];
-     
+
 }
 
 
 #pragma mark - 定时器
-- (void)timerFired
-{
-    pageNumnber ++;
+
+- (void)timerFired {
+    pageNumnber++;
     if (pageNumnber == self.model.pics.count) {
         [self.centerDetailView.scrollerView setContentOffset:CGPointMake(0, 0) animated:YES];
 
         pageNumnber = 0;
-    }else {
-        
+    } else {
+
         [self.centerDetailView.scrollerView setContentOffset:CGPointMake(pageNumnber * kScreenSize.width, 0) animated:YES];
 
     }
-    
-    
-    self.pageControl.currentPage = pageNumnber ;
-    
-    
-    
+
+
+    self.pageControl.currentPage = pageNumnber;
+
+
 }
 
 #pragma mark - 请求数据
 
-- (void)getData
-{
-    
+- (void)getData {
+
     self.manager = [AFHTTPRequestOperationManager manager];
     self.manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    
-    NSDictionary *parameter = @{@"store_id":self.storeId};
+
+    NSDictionary *parameter = @{@"store_id" : self.storeId};
     NSDictionary *signParameter = [ODAPIManager signParameters:parameter];
-    
-    NSString *url = @"http://woquapi.odong.com/1.0/other/store/detail";
-    
-    __weak typeof (self)weakSelf = self;
-    [self.manager GET:url parameters:signParameter success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
-        
+    NSString *url = [ODAPIManager getUrl:@"/1.0/other/store/detail"];
+
+    __weak typeof(self) weakSelf = self;
+    [self.manager GET:url parameters:signParameter success:^(AFHTTPRequestOperation *_Nonnull operation, id _Nonnull responseObject) {
+
         if (responseObject) {
             NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingMutableContainers error:nil];
             NSMutableDictionary *result = dict[@"result"];
@@ -158,20 +153,20 @@ int pageNumnber = 0;
             weakSelf.model = [[CenterDetailModel alloc] initWithDict:result];
 
             [weakSelf createTableView];
-            
+
             [weakSelf.tableView reloadData];
-           
+
         }
-    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
-        
-        
+    }         failure:^(AFHTTPRequestOperation *_Nullable operation, NSError *_Nonnull error) {
+
+
     }];
 }
 
 
 #pragma mark - 初始化
-- (void)createTableView
-{
+
+- (void)createTableView {
     self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, ODTopY, kScreenSize.width, kScreenSize.height - 60) style:UITableViewStylePlain];
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
@@ -179,90 +174,83 @@ int pageNumnber = 0;
     self.tableView.backgroundColor = [UIColor colorWithHexString:@"#e6e6e6" alpha:1];
     self.tableView.separatorStyle = UITableViewCellSelectionStyleNone;
     [self.view addSubview:self.tableView];
-    
-    
-    
+
+
 }
 
 
-
-
 #pragma mark - 懒加载
-- (ODCenderDetailView *)centerDetailView
-{
+
+- (ODCenderDetailView *)centerDetailView {
     if (_centerDetailView == nil) {
-        
-        
-        CGRect rect = [self.model.desc boundingRectWithSize:CGSizeMake([UIScreen mainScreen].bounds.size.width - 20,0)
-                                                    options:NSStringDrawingUsesLineFragmentOrigin |NSStringDrawingUsesFontLeading
+
+
+        CGRect rect = [self.model.desc boundingRectWithSize:CGSizeMake([UIScreen mainScreen].bounds.size.width - 20, 0)
+                                                    options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
                                                  attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:11]}
                                                     context:nil];
-        
+
         self.centerDetailView = [ODCenderDetailView getView];
-        
-              
+
+
         if (iPhone4_4S) {
-            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 10 +  self.centerDetailView.scrollerHeight.constant +  rect.size.height);
-            
-        }else if (iPhone5_5s) {
-            
-            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 12 +  self.centerDetailView.scrollerHeight.constant +  rect.size.height);
-            
-        }else if (iPhone6_6s) {
-            
-            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 14 +  self.centerDetailView.scrollerHeight.constant +  rect.size.height);
-            
-            
-        }else {
-            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 16 +  self.centerDetailView.scrollerHeight.constant +  rect.size.height);
-            
+            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 10 + self.centerDetailView.scrollerHeight.constant + rect.size.height);
+
+        } else if (iPhone5_5s) {
+
+            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 12 + self.centerDetailView.scrollerHeight.constant + rect.size.height);
+
+        } else if (iPhone6_6s) {
+
+            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 14 + self.centerDetailView.scrollerHeight.constant + rect.size.height);
+
+
+        } else {
+            self.centerDetailView.frame = CGRectMake(0, 0, kScreenSize.width, 250 + kScreenSize.height / 16 + self.centerDetailView.scrollerHeight.constant + rect.size.height);
+
         }
-        
-        
-        
+
+
         self.centerDetailView.detailTextView.text = self.model.desc;
         self.centerDetailView.centerNameLabel.text = self.model.name;
         self.centerDetailView.phoneLabel.text = self.model.tel;
         self.centerDetailView.addressTextView.text = self.model.address;
         self.centerDetailView.addressTextView.scrollEnabled = NO;
         self.centerDetailView.timeTextView.text = self.model.business_hours;
-        
-        
-        
-        
-        
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(phoneAction:)];
+
+
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(phoneAction:)];
         [self.centerDetailView.phoneLabel addGestureRecognizer:tap];
-        
+
         [self.centerDetailView.appointmentButton addTarget:self action:@selector(appointmentAction:) forControlEvents:UIControlEventTouchUpInside];
-        
-        
-        UITapGestureRecognizer *addressTap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(addressAction:)];
+
+
+        UITapGestureRecognizer *addressTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(addressAction:)];
         [self.centerDetailView.addressImageView addGestureRecognizer:addressTap];
-        
-     
-        
-        
+
+
+
+
         // 设置scrollerView的内容视图大小
-        self.centerDetailView.scrollerView.contentSize = CGSizeMake(self.model.pics.count * kScreenSize.width,  self.centerDetailView.scrollerHeight.constant);
+        self.centerDetailView.scrollerView.contentSize = CGSizeMake(self.model.pics.count * kScreenSize.width, self.centerDetailView.scrollerHeight.constant);
         // 设置整页滑动
-         self.centerDetailView.scrollerView.pagingEnabled = YES;
+        self.centerDetailView.scrollerView.pagingEnabled = YES;
         // 隐藏滚动条
         self.centerDetailView.scrollerView.showsHorizontalScrollIndicator = NO;
         self.centerDetailView.scrollerView.delegate = self;
 
         // 创建图片
         for (int i = 0; i < self.model.pics.count; i++) {
-            
+
             // 创建imageView;
             UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(i * kScreenSize.width, 0, kScreenSize.width, self.centerDetailView.scrollerHeight.constant)];
             NSString *url = self.model.pics[i];
             [imageView sd_setImageWithURL:[NSURL OD_URLWithString:url]];
             // imageView添加到scroller上
             [self.centerDetailView.scrollerView addSubview:imageView];
-            
+
         }
-        
+
         // 创建pageControl
         self.pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(self.view.center.x - 50, self.centerDetailView.scrollerHeight.constant - 30, 100, 20)];
         self.pageControl.backgroundColor = [UIColor clearColor];
@@ -276,38 +264,31 @@ int pageNumnber = 0;
         self.pageControl.pageIndicatorTintColor = [UIColor lightGrayColor];
         self.pageControl.userInteractionEnabled = YES;
         [self.centerDetailView addSubview:self.pageControl];
-        
-        
-        
-        
+
+
     }
     return _centerDetailView;
 }
 
 #pragma mark - UITableViewDelegate
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     NSString *cellID = @"cellId";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
     }
-    
-    
+
+
     return cell;
 }
 
 
-
-
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 0;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 0;
 }
 
