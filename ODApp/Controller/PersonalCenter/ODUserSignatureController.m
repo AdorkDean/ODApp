0000f43a0ce02e564809e58fa156d5786b688de1
wanghuida <wanghuida258@aliyun.com>
2016-03-02 12:26:30 +0800

    修改签名

--- ODApp/Controller/PersonalCenter/ODUserSignatureController.m
+++ ODApp/Controller/PersonalCenter/ODUserSignatureController.m
@@ -9,11 +9,12 @@
 #import "ODUserSignatureController.h"
 #import "AFNetworking.h"
 #import "ODAPIManager.h"
+#import "Masonry.h"
+#import "ODUserResponse.h"
 
 @interface ODUserSignatureController ()<UITextViewDelegate>
 
 @property (nonatomic , strong) UITextView *textView;
-@property(nonatomic,strong) AFHTTPRequestOperationManager *manager;
 
 @end
 
@@ -31,28 +32,28 @@
     // 注册button
     self.navigationItem.rightBarButtonItem = [UIBarButtonItem OD_itemWithTarget:self action:@selector(registered:) color:nil highColor:nil title:@"保存"];
 }
+
 - (void)creatTextView
 {
-    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(4, 4, kScreenSize.width - 8, 30)];
+    self.textView = [[UITextView alloc] init];
+    [self.view addSubview:self.textView];
+    
+    [self.textView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).with.offset(10);
+        make.left.equalTo(self.view).with.offset(0);
+        make.right.equalTo(self.view).with.offset(0);
+        make.height.mas_equalTo(@80);
+    }];
     self.textView.backgroundColor = [UIColor whiteColor];
-    self.textView.layer.masksToBounds = YES;
-    self.textView.layer.cornerRadius = 5;
-    self.textView.layer.borderColor = [UIColor colorWithHexString:@"#d0d0d0" alpha:1].CGColor;
-    self.textView.layer.borderWidth = 1;
     
     if ([self.signature isEqualToString:@"未设置签名"] || [self.signature isEqualToString:@"请输入签名"]) {
         self.textView.text = @"请输入签名";
         self.textView.textColor = [UIColor lightGrayColor];
-
-    }else {
+    } else {
         self.textView.text = self.signature;
     }
-    
     self.textView.font = [UIFont systemFontOfSize:14];
-    
     self.textView.delegate = self;
-    [self.view addSubview:self.textView];
-    
 }
 
 
@@ -97,37 +98,21 @@
 
 - (void)registered:(UIButton *)sender
 {
-     NSString *openID = [ODUserInformation sharedODUserInformation].openID;
     
-    self.manager = [AFHTTPRequestOperationManager manager];
+    ODUser *user = [[ODUserInformation sharedODUserInformation] getUserCache];
+    NSDictionary *parameters = @{@"user_sign":self.textView.text , @"open_id":user.open_id};
     
-    NSDictionary *parameters = @{@"user_sign":self.textView.text , @"open_id":openID};
-    NSDictionary *signParameters = [ODAPIManager signParameters:parameters];
-    __weakSelf
-    
-  
-    [self.manager GET:kChangeUserInformationUrl parameters:signParameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [ODAPIManager getWithURL:@"/user/change" params:parameters success:^(id responseObject) {
         
+        [ODProgressHUD showToast:self.view msg:@"修改成功"];
+        ODUserResponse *resp = [ODUserResponse mj_objectWithKeyValues:responseObject];
+        ODUser *user = resp.result;
+        [[ODUserInformation sharedODUserInformation] updateUserCache:user];
         
-        if ([responseObject[@"status"]isEqualToString:@"success"]) {
-            if (weakSelf.getTextBlock) {
-                if (weakSelf.getTextBlock) {
-                    weakSelf.getTextBlock(self.textView.text);
-                }
-                
-                [weakSelf.navigationController popViewControllerAnimated:YES];
-
-            }
-        }
-        
-        else if ([responseObject[@"status"]isEqualToString:@"error"]) {
-                      
-            [ODProgressHUD showInfoWithStatus:responseObject[@"message"]];
-            
-        }
-      
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        
+        [self.navigationController popViewControllerAnimated:YES];
+    } error:^(NSString *msg) {
+        [ODProgressHUD showToast:self.view msg:msg];
+    } failure:^(NSError *error) {
         
     }];
 }
