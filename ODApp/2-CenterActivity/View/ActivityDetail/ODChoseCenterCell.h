3afa61fb174c6920cecbc32086167735c5e3f5cc
代征钏 <1036739617@qq.com>
2016-03-11 15:32:43 +0800

    choseCenter

---
+++ ODApp/2-CenterActivity/View/ActivityDetail/ODChoseCenterCell.h
@@ -0,0 +1,13 @@
+//
+//  ODChoseCenterCell.h
+//  ODApp
+//
+//  Created by Bracelet on 16/3/11.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODChoseCenterCell : UITableViewCell
+
+@end
---
+++ ODApp/2-CenterActivity/View/ActivityDetail/ODChoseCenterCell.m
@@ -0,0 +1,23 @@
+//
+//  ODChoseCenterCell.m
+//  ODApp
+//
+//  Created by Bracelet on 16/3/11.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import "ODChoseCenterCell.h"
+
+@implementation ODChoseCenterCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
