6be9cd567b3fb90a0ff2d5f942d29c13e2d65e3b
代征钏 <1036739617@qq.com>
2016-03-25 17:16:10 +0800

    bug

--- ODApp/5-PersonalCenter/Controller/Personal/ODMyReplyTopicViewController.m
+++ ODApp/5-PersonalCenter/Controller/Personal/ODMyReplyTopicViewController.m
@@ -112,9 +112,9 @@
         [weakSelf.tableView reloadData];
         [ODHttpTool od_endRefreshWith:weakSelf.tableView array:[[model result] bbs_list]];
         if (weakSelf.dataArray.count == 0) {
-            [weakSelf.noResultabel showOnSuperView:weakSelf.tableView title:@"暂无话题"];
+            [weakSelf.noResultLabel showOnSuperView:weakSelf.tableView title:@"暂无话题"];
         }else {
-            [weakSelf   .noResultabel hidden];
+            [weakSelf.noResultLabel hidden];
         }
     } failure:^(NSError *error) {
         [weakSelf.tableView.mj_header endRefreshing];
