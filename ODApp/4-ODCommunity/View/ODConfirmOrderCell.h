6e75a29da6fc15de455a8d82b90461c451431d95
Odong-YG <13283753532@163.com>
2016-03-31 16:31:45 +0800

    调试订单接口

---
+++ ODApp/4-ODCommunity/View/ODConfirmOrderCell.h
@@ -0,0 +1,13 @@
+//
+//  ODConfirmOrderCell.h
+//  ODApp
+//
+//  Created by Odong-YG on 16/3/31.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODConfirmOrderCell : UITableViewCell
+
+@end
