e95a3b051bde516a3b764c6192531817983343c5
王振航 <wang_4727@126.com>
2016-03-31 21:08:30 +0800

    购物车

---
+++ ODApp/1-HomeFind/View/ODShopCartListCell.h
@@ -0,0 +1,13 @@
+//
+//  ODShopCartListCell.h
+//  ODApp
+//
+//  Created by 王振航 on 16/3/31.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODShopCartListCell : UITableViewCell
+
+@end
---
+++ ODApp/1-HomeFind/View/ODShopCartListCell.m
@@ -0,0 +1,23 @@
+//
+//  ODShopCartListCell.m
+//  ODApp
+//
+//  Created by 王振航 on 16/3/31.
+//  Copyright © 2016年 Odong Org. All rights reserved.
+//
+
+#import "ODShopCartListCell.h"
+
+@implementation ODShopCartListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
