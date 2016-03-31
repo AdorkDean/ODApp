b459c1eda064097c6917095c617309e26c462777
代征钏 <1036739617@qq.com>
2016-03-31 21:07:28 +0800

    卖品单页

--- ODApp/1-HomeFind/Controller/ODTakeAwayDetailController.m
+++ ODApp/1-HomeFind/Controller/ODTakeAwayDetailController.m
@@ -5,12 +5,17 @@
 //  Created by Bracelet on 16/3/24.
 //  Copyright © 2016年 Odong Org. All rights reserved.
 //
+#define MAS_SHORTHAND
+#define MAS_SHORTHAND_GLOBALS
 
 #import "ODTakeAwayDetailController.h"
 
+#import <Masonry.h>
 #import "ODHttpTool.h"
 #import "ODUserInformation.h"
 #import "ODAPPInfoTool.h"
+
+#import "ODShopCartView.h"
 
 @interface ODTakeAwayDetailController ()
 @property (nonatomic, strong) PontoDispatcher *pontoDispatcher;
@@ -21,6 +26,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = self.takeAwayTitle;
+    [self setupShopCart];
     self.pontoDispatcher = [[PontoDispatcher alloc] initWithHandlerClassesPrefix:@"Ponto" andWebView:self.webView];
     if (self.isCart) {
         NSString *urlString = [[ODHttpTool getRequestParameter:@{@"open_id":@"766148455eed214ed1f8"}]od_URLDesc];
@@ -28,9 +34,17 @@
         [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL OD_URLWithString:url]]];
     }
     else {
-        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@?id=%@", ODWebUrlNative, self.product_id]]]];
+        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@?id=1", ODWebUrlNative]]]];
     }    
 }
 
-
+- (void)setupShopCart
+{
+    ODShopCartView *shopCart = [ODShopCartView shopCart];
+    [self.view addSubview:shopCart];
+    [shopCart makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.left.right.equalTo(self.view);
+        make.height.equalTo(55);
+    }];
+}
 @end
